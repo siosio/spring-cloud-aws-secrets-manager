fb3219972a04e01a0bd655a4bf4474343e0c5cf8
Uwe Schaefer <uwe.schaefer@mercateo.com>
2018-07-25 15:17:12 +0200

    prefixing of keys with 'secret_name.'

--- src/main/java/com/github/siosio/AwsSecretsManagerEnvironmentPostProcessor.java
+++ src/main/java/com/github/siosio/AwsSecretsManagerEnvironmentPostProcessor.java
@@ -1,7 +1,14 @@
 package com.github.siosio;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
 
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
@@ -9,11 +16,6 @@ import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import com.amazonaws.services.secretsmanager.model.ListSecretsRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
 
 public class AwsSecretsManagerEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
@@ -36,12 +38,23 @@ public class AwsSecretsManagerEnvironmentPostProcessor implements EnvironmentPos
               .getSecretList()
               .forEach(entry ->
                       {
+
                           final String name = entry.getName();
                           final GetSecretValueResult result = client.getSecretValue(
                                   new GetSecretValueRequest().withSecretId(name));
-                          propertySources.addFirst(new MapPropertySource(name, jsonToMap(result.getSecretString())));
+                          Map<String, Object> jsonToMap = jsonToMap(result.getSecretString());
+                          propertySources.addFirst(new MapPropertySource(name, jsonToMap));
+                          propertySources.addFirst(new MapPropertySource("prefixed_"+name, prefixKeys(name,jsonToMap)));
                       }
               );
+    }
+
+    private Map<String, Object> prefixKeys(String name, Map<String, Object> jsonToMap) {
+        HashMap<String, Object> mapWithPrefixedKeys = new HashMap<>(jsonToMap.size());
+        jsonToMap.entrySet().forEach(e->{
+            mapWithPrefixedKeys.put(name+"."+e.getKey(), e.getValue());
+        });
+        return mapWithPrefixedKeys;
     }
 
     private Map<String, Object> jsonToMap(String jsonString) {
