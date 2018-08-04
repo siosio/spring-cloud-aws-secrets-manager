e5b2009e760fe71cfdf924765f9230ce787771f2
siosio <siosio12@gmail.com>
2018-08-04 16:18:39 +0900

    fix test

--- src/test/java/com/github/siosio/IntegrationTest.java
+++ src/test/java/com/github/siosio/IntegrationTest.java
@@ -3,6 +3,7 @@ package com.github.siosio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
@@ -15,24 +16,43 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = IntegrationTest.Comp.class)
+@SpringBootTest(classes = {IntegrationTest.Comp.class, IntegrationTest.Properties.class})
 public class IntegrationTest {
     
     @Autowired
-    public Comp comp;
+    private Comp comp;
+    
+    @Autowired
+    private Properties properties; 
 
     @Test
     public void injectFromAWSSecretsManager() {
         System.out.println("******************************");
-        System.out.println("app.testValue = " + comp.testValue);
+        System.out.println("comp.testValue = " + comp.testValue);
+        System.out.println("properties.getId() = " + properties.getId());
         System.out.println("******************************");
     }
     
     @SpringBootApplication
     @Component
     public static class Comp {
-        @Value("${prod/test-data.test.id}")
-        public String testValue;
+        @Value("${prod.test-data.test.id}")
+        private String testValue;
+    }
+    
+    @Component
+    @ConfigurationProperties(prefix = "prod.test-data.test")
+    public static class Properties {
+        
+        private String id;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(final String id) {
+            this.id = id;
+        }
     }
 
 }
